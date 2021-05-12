4b1745192045e88adb0624c19a786313e0b08ce3
Smit Patel <sbhikadiya892@rku.ac.in>
2021-05-12 18:37:15 +0530

    Design Process is almost done

---
+++ static/js/dashboard.js
@@ -0,0 +1,12 @@
+
+document.getElementById("accountDetailes").addEventListener("click",function(){
+   document.getElementById("myFrame").setAttribute("src","accountDetailesForm.html");
+});;
+
+document.getElementById("walletCash").addEventListener("click",function(){
+   document.getElementById("myFrame").setAttribute("src","checkBalance.html");
+});;
+
+document.getElementById("getCash").addEventListener("click",function(){
+   document.getElementById("myFrame").setAttribute("src","withDrawCash.html");
+});;
